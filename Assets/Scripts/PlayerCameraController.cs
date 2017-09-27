1f29f465ce9d724181eb67e4f67f04892bf59f99
swood456 <crazedsportsfan@gmail.com>
2017-09-27 16:58:38 -0400

    :dog: put in a lot of Unity standard assets to make a better doggo controller

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour {
+
+    public GameObject toFollow;
+    
+
+    private void Start()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+}
--- Assets/Scripts/PuppyController.cs
+++ Assets/Scripts/PuppyController.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class PuppyController : MonoBehaviour {
 
+    public float moveSpeed = 3.0f;
+    public float rotSpeed = 5.0f;
+    public float cameraRotSpeed = 2.0f;
+    public Camera m_camera;
+    Rigidbody rb;
+    
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rb = GetComponent<Rigidbody>();
+        /*
+        Cursor.lockState = CursorLockMode.Locked;
+        */
+    }
+
+    void swapCameraMode()
+    {
+        if(Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        // move the dog
+        float vertical = Input.GetAxis("Vertical");
+        float horiztonal = Input.GetAxis("Horizontal");
+
+        // rotate the doggo
+        rb.angularVelocity = new Vector3(0, horiztonal * rotSpeed, 0);
+        
+
+        rb.velocity = transform.forward * vertical * moveSpeed;
+    }
 }
