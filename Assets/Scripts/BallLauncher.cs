0a6ea64613fd610d5ed082d235f7858a07a8d5d7
reldredge10 <reldredge10@gmail.com>
2017-09-28 21:26:34 -0400

    The ball launcher launches balls!

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour {
+
+    [SerializeField] private Vector3 launchDirection;           //direction of ball launch
+    [SerializeField] private float launchForce;                 //force of launch, applied in direction specified above
+
+    [SerializeField] private Vector3 storePosition;             //position within ball launcher to store it until launch
+
+    private GameObject ballForLaunch = null;                    //ball about to be launched
+
+    [SerializeField] private float waitTime;                    //time between ball being loaded and ball being launched
+    private float timer = 0f;                                   //current time from load to launch
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(ballForLaunch != null)
+		{
+            ballForLaunch.transform.rotation = this.transform.rotation;
+            ballForLaunch.transform.localPosition = storePosition;
+
+            timer += Time.deltaTime;
+            if (timer >= waitTime)
+                LaunchBall();
+        }
+	}
+
+
+    public void LoadBall( GameObject ball)
+    {
+        ballForLaunch = ball;
+        ballForLaunch.transform.parent = this.transform;
+        ballForLaunch.transform.localPosition = storePosition;
+        ballForLaunch.GetComponent<Rigidbody>().useGravity = false;
+        timer = 0f;
+    }
+
+    void LaunchBall()
+    {
+        ballForLaunch.transform.parent = null;
+        ballForLaunch.GetComponent<Rigidbody>().useGravity = true;
+        ballForLaunch.GetComponent<Rigidbody>().AddForce(launchDirection * launchForce, ForceMode.Impulse);
+        ballForLaunch = null;
+    }
+}
--- Assets/Scripts/PuppyPickup.cs
+++ Assets/Scripts/PuppyPickup.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PuppyPickup : MonoBehaviour {
 
-    private GameObject itemInMouth;                 //reference to item currently in the dog's mouth
-    [SerializeField] private Transform mouth;       //location of the mouth to move items to
+    private GameObject itemInMouth = null;                                      //reference to item currently in the dog's mouth
+    private Vector3 prevPosition = new Vector3(0f, 0f, 0f);                     //when ball is let go, this is used to calculate it's momentum
+    [SerializeField] private Transform mouth;                                   //location of the mouth to move items to
 
-    private List<GameObject> objectsInRange;        //objects in pickup range
+    private List<GameObject> objectsInRange = new List<GameObject>();           //objects in pickup range
+    private BallLauncher launcherInRange = null;                                //ball launcher that is in range (if one exists)
 
 
 	// Use this for initialization
 	void Start () {
-        itemInMouth = null;
-        objectsInRange = new List<GameObject>();
+        //itemInMouth = null;
+        //objectsInRange = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
@@ -21,14 +23,28 @@ public class PuppyPickup : MonoBehaviour {
         //determine if E key has been pressed.  E key is used to pickup and drop objects
         if(Input.GetKeyDown(KeyCode.E))
         {
-            //first, if there is an object in the dog's mouth, drop it
+            //first, if there is an object in the dog's mouth, drop it or load it into a launcher
             if(itemInMouth != null)
             {
-                itemInMouth.transform.parent = null;
-                itemInMouth.GetComponent<Rigidbody>().useGravity = true;
+                //first check if we're in range of a ball launcher.  If so, load it
+                if (launcherInRange && itemInMouth.name == "Tennis Ball")
+                {
+                    Debug.Log("Loading Ball");
+                    launcherInRange.LoadBall(itemInMouth);
+                }
+                //Otherwise, drop it
+                else
+                {
+                    itemInMouth.transform.parent = null;
+                    itemInMouth.GetComponent<Rigidbody>().useGravity = true;
+                    //calculate the momentum of the ball due to the dog's turning.
+                    //If you don't it will always fall straight to the ground
+                    itemInMouth.GetComponent<Rigidbody>().velocity = (itemInMouth.transform.position - prevPosition) / Time.deltaTime;
+                }
                 itemInMouth = null;
             }
-            //otherwise, see if there are objects in range adn pick up the closest one
+
+            //otherwise, see if there are objects in range and pick up the closest one
             else if(objectsInRange.Count > 0)
             {
                 itemInMouth = ClosestObject();
@@ -37,6 +53,10 @@ public class PuppyPickup : MonoBehaviour {
                 itemInMouth.GetComponent<Rigidbody>().useGravity = false;
             }
         }
+
+        //update prevPosition
+        if (itemInMouth != null)
+            prevPosition = itemInMouth.transform.position;
 	}
 
     private void LateUpdate()
@@ -54,19 +74,33 @@ public class PuppyPickup : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        //if it's a pickup item, add it to the list
         if (other.tag == "Pickup" && !objectsInRange.Contains(other.gameObject))
         {
             Debug.Log("Object In");
             objectsInRange.Add(other.gameObject);
         }
+        //if it's a ball launcher, store it in launcherInRange
+        else if (other.tag == "Launcher")
+        {
+            Debug.Log("Launcher in range");
+            launcherInRange = other.GetComponent<BallLauncher>();
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //if it's a pickup item, remove it from the list
         if (other.tag == "Pickup" && objectsInRange.Contains(other.gameObject))
         {
             Debug.Log("Object Out");
             objectsInRange.Remove(other.gameObject);
+        }
+        //if it's a ball launcher, set launcherInRange to null
+        else if (other.tag == "Launcher")
+        {
+            Debug.Log("Launcher out of range");
+            launcherInRange = null;
         }
     }
 
