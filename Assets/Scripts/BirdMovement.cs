fdb556e14778717dafff12d736efa003fc7d7ba5
Wood <woods5@win.rpi.edu>
2017-10-04 02:59:14 -0400

    made the bird flyyy

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -29,6 +29,8 @@ public class BirdMovement : MonoBehaviour {
     [Header("Bath stats")]
     public float dist_for_bath_runaway;
 
+    private Animator m_animator;
+
     BirdState m_state;
     float time_in_state = 0.0f;
     Rigidbody rb;
@@ -38,8 +40,18 @@ public class BirdMovement : MonoBehaviour {
         Dog = GameObject.FindGameObjectWithTag("Player");
         flight_vector.Normalize();
 
+        m_animator = GetComponent<Animator>();
+
         // animation state is wander/hop
-	}
+        AnimationClip clip;
+
+        AnimationEvent evt = new AnimationEvent();
+
+        evt.time = 0.0f;
+        evt.functionName = "hop_forward";
+        clip = m_animator.runtimeAnimatorController.animationClips[0];
+        clip.AddEvent(evt);
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -65,11 +77,13 @@ public class BirdMovement : MonoBehaviour {
                 time_in_state = 0.0f;
             m_state = BirdState.FlyAway;
             // animation state is flying
+            m_animator.SetBool("flying", true);
         }
         if ((m_state == BirdState.BathMode || m_state == BirdState.FlyDown) && dist_to_dog < dist_for_bath_runaway)
         {
             m_state = BirdState.FlyAway;
             // animation state is flying (is there an issue if we go from fly->fly?)
+            m_animator.SetBool("flying", true);
         }
 
     }
@@ -79,7 +93,7 @@ public class BirdMovement : MonoBehaviour {
         switch(m_state)
         {
             case BirdState.Wander:
-                WanderMove();
+                //WanderMove();
                 break;
             case BirdState.FlyAway:
                 FlyAway();
@@ -93,6 +107,19 @@ public class BirdMovement : MonoBehaviour {
         }
     }
 
+    void hop_forward()
+    {
+        if(m_state == BirdState.Wander)
+        {
+            Vector3 wander_circle_center = transform.right * -1 * wander_circle_center_dist;
+            Vector2 circle_pos = ((Vector3)Random.insideUnitCircle.normalized * wander_circle_radius);
+            Vector3 wander_dest = wander_circle_center + new Vector3(circle_pos.x, 0, circle_pos.y);
+
+            transform.right = -wander_dest.normalized;
+            rb.AddForce(wander_dest.normalized * hop_force);
+        }
+    }
+
 
     void WanderMove()
     {
@@ -102,11 +129,11 @@ public class BirdMovement : MonoBehaviour {
             time_in_state = 0.0f;
 
             // find a direction to hop in
-            Vector3 wander_circle_center = transform.forward * wander_circle_center_dist;
+            Vector3 wander_circle_center = transform.right * -1 * wander_circle_center_dist;
             Vector2 circle_pos = ((Vector3)Random.insideUnitCircle.normalized * wander_circle_radius);
             Vector3 wander_dest = wander_circle_center + new Vector3(circle_pos.x, 0, circle_pos.y);
 
-            transform.forward = wander_dest.normalized;
+            transform.right = -wander_dest.normalized;
             rb.AddForce(wander_dest.normalized * hop_force);
         }
         else
@@ -124,18 +151,21 @@ public class BirdMovement : MonoBehaviour {
         }
 
         rb.velocity = flight_vector * flight_speed;
+        transform.right = -flight_vector;
     }
 
     void FlyDown()
     {
         rb.velocity = (bird_bath_pos.position - transform.position).normalized * flight_speed;
-        if((bird_bath_pos.position - transform.position).magnitude < 0.1f)
+        transform.right = -rb.velocity.normalized;
+        if ((bird_bath_pos.position - transform.position).magnitude < 0.1f)
         {
             m_state = BirdState.BathMode;
             rb.velocity = Vector3.zero;
 
             // set animation state to sitting on birdbath
             // can also do some stuff with getting him to play in the bath
+            m_animator.SetBool("flying", false);
         }
     }
 
