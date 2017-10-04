f1aacf43c31a806908b138e30941d0a66c8ee060
Wood <woods5@win.rpi.edu>
2017-10-04 03:29:08 -0400

    made main menu

--- Assets/Scripts/MainScreenController.cs
+++ Assets/Scripts/MainScreenController.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScreenController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void go_next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
