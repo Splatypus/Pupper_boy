75e1ad87b3b5e16694539c0bd34445226b145593
Robert <reldredge10@gmail.com>
2017-10-04 02:01:06 -0400

    We have takeoff

---
+++ Assets/Scripts/FlightMode.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class FlightMode : MonoBehaviour {
+
+    [SerializeField] private bool isFlying;
+    [SerializeField] private float speedOfFlight;
+    [SerializeField] private float takeoffAngle;
+    [SerializeField] private float tiltSensitivity;
+
+    private Rigidbody rb;
+    private TrailRenderer tr;
+    private ParticleSystem ps;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        tr = this.GetComponent<TrailRenderer>();
+        ps = this.GetComponent<ParticleSystem>();
+
+        tr.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFlying)
+        {
+            float vertical = Input.GetAxis("Vertical") * tiltSensitivity;
+            float horiztonal = Input.GetAxis("Horizontal") * tiltSensitivity;
+
+            this.transform.Rotate(new Vector3(-1f * vertical, horiztonal, 0f));
+            rb.velocity = transform.forward * speedOfFlight;
+        }
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if (isFlying)
+                DeactivateFlightMode();
+            else
+                ActivateFlightMode();
+        }
+	}
+
+    public void ActivateFlightMode()
+    {
+        this.GetComponent<Rigidbody>().useGravity = false;
+        this.GetComponent<ThirdPersonUserControl>().enabled = false;
+        tr.enabled = true;
+        ps.Play();
+        this.transform.Rotate(new Vector3(-1f * takeoffAngle, 0f, 0f));
+        isFlying = true;
+    }
+
+    public void DeactivateFlightMode()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+        this.GetComponent<ThirdPersonUserControl>().enabled = true;
+        tr.enabled = false;
+        ps.Stop();
+        isFlying = false;
+    }
+
+    public bool IsFlying()
+    {
+        return isFlying;
+    }
+}
--- Assets/Scripts/PuppyController.cs
+++ Assets/Scripts/PuppyController.cs
@@ -9,6 +9,8 @@ public class PuppyController : MonoBehaviour {
     public float cameraRotSpeed = 2.0f;
     public Camera m_camera;
     Rigidbody rb;
+
+    public FlightMode flightMode;
     
 	// Use this for initialization
 	void Start () {
@@ -32,15 +34,17 @@ public class PuppyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (!flightMode.IsFlying())
+        {
+            // move the dog
+            float vertical = Input.GetAxis("Vertical");
+            float horiztonal = Input.GetAxis("Horizontal");
 
-        // move the dog
-        float vertical = Input.GetAxis("Vertical");
-        float horiztonal = Input.GetAxis("Horizontal");
+            // rotate the doggo
+            rb.angularVelocity = new Vector3(0, horiztonal * rotSpeed, 0);
 
-        // rotate the doggo
-        rb.angularVelocity = new Vector3(0, horiztonal * rotSpeed, 0);
-        
 
-        rb.velocity = transform.forward * vertical * moveSpeed;
+            rb.velocity = transform.forward * vertical * moveSpeed;
+        }
     }
 }
