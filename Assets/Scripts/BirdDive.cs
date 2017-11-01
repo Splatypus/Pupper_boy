47ba6da1eba868f2acb2da986856645d40c06b56
Rikusaber <huange2@rpi.edu>
2017-11-01 11:09:12 -0400

    bird scripts

---
+++ Assets/Scripts/BirdDive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdDive : MonoBehaviour {
+
+    public GameObject objectA;
+    public GameObject objectB;
+
+
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(objectA.transform.position, objectB.transform.position, Mathf.PingPong(Time.time, 1));
+    }
+
+}
