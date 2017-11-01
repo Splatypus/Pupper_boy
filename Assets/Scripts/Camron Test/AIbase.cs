b787a0feccabd22461364f29dbc36832beebeea9
Splatypus <lionheart365@gmail.com>
2017-11-01 01:48:53 -0400

    NPC says stuff when you get close or bring them a toy

--- Assets/Scripts/Camron Test/AIbase.cs
+++ Assets/Scripts/Camron Test/AIbase.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class AIbase : MonoBehaviour {
 
-    public Sprite[] Dialog;
+    public GameObject[] Dialog;
     public GameObject Player;
     public float range;
+    public GameObject Toy;
 
     private bool inRange = false;
+    private GameObject activeIcon;
     
 
     //Triggers when the player enters the range
     public virtual void OnInRange() {
-
+        Display(Dialog[0]);
     }
     //Trigger when the player leaves range
     public virtual void OnExitRange() {
-
+        EndDisplay();
     }
+
+    public virtual void ToyInRange() {
+        Destroy(Toy);
+        EndDisplay();
+        Display(Dialog[1]);
+    }
+
     //Trigger when the player barks while in rage
     public virtual void OnBark() {
 
     }
     //Displays the thought bubble
-    public void Display(Sprite icon) {
-
+    public void Display(GameObject icon) {
+        EndDisplay();
+        icon.SetActive(true);
+        activeIcon = icon;
     }
     //hides the icon
     public void EndDisplay() {
-
+        if (activeIcon != null) {
+            activeIcon.SetActive(false);
+        }
     }
 	
 	// Update is called once per frame
@@ -41,12 +54,17 @@ public class AIbase : MonoBehaviour {
             inRange = false;
             OnExitRange();
         }
-	}
+        if (Toy != null && Vector3.Distance(gameObject.transform.position, Toy.transform.position) < range) {
+            ToyInRange();
+        }
+    }
 
 
     // Use this for initialization
     void Start() {
-
+        foreach (GameObject i in Dialog) {
+            i.SetActive(false);
+        }
     }
 
 }
---
+++ Assets/Scripts/Camron Test/BillboardScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+	}
+}
