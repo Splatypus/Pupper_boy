0c325368d0619e97f0bc1efabc552183d63c0cb7
Splatypus <lionheart365@gmail.com>
2017-11-12 01:57:45 -0500

    Minigame manager class

---
+++ Assets/Scripts/Camron Test/MiniGameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameManager : MonoBehaviour {
+
+    public GameObject relatedNPC;
+    public float timeLimit;
+
+
+    public ArrayList highscores = new ArrayList();
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
