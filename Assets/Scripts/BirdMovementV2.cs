ac4ac78b226a8ee26d5a2817185a061069ebc114
swood456 <crazedsportsfan@gmail.com>
2017-12-05 15:04:15 -0500

    fixed bug where birds were not turning around correctly

--- Assets/Scripts/BirdMovementV2.cs
+++ Assets/Scripts/BirdMovementV2.cs
@@ -71,17 +71,19 @@ public class BirdMovementV2 : MonoBehaviour {
     {
         // PLACEHOLDER
         //transform.right = desiredRight;
-        if(Vector3.Distance(transform.right, desiredRight) > float.Epsilon)
+        if(curState == BirdState.FlyWander || curState == BirdState.AttackWander)
         {
-           // print("doing lerp t = " + t);
-            t += Time.deltaTime;
-            transform.right = Vector3.Lerp(transform.right, desiredRight, t / 50);
+            if (Vector3.Distance(transform.right, desiredRight) > float.Epsilon)
+            {
+                //print("doing lerp t = " + t);
+                t += Time.deltaTime;
+                transform.right = Vector3.Lerp(transform.right, desiredRight, t / 50);
+            }
+            else
+            {
+                t = 0.0f;
+            }
         }
-        else
-        {
-            t = 0.0f;
-        }
-        //transform.right = Vector3.Lerp()
 
         // when wandering all we have to do is watch out for the dog, movement is controlled by animation events
         if( (curState == BirdState.Wander || curState == BirdState.FlyDown || curState == BirdState.BathMode || curState == BirdState.AttackWander) &&
