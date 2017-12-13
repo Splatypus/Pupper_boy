5d809a4850296882a2be525b69922da7618f140b
swood456 <crazedsportsfan@gmail.com>
2017-12-13 01:44:10 -0500

    update control menu

---
+++ Assets/Scripts/ControlMenuImageV2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlMenuImageV2 : MonoBehaviour {
+
+    public Image control_image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            control_image.gameObject.SetActive(!control_image.gameObject.activeSelf);
+        }
+    }
+}
