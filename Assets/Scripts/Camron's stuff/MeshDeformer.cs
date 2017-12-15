3922fa5f9f9243b3c45896137e3af24347600417
Splatypus <lionheart365@gmail.com>
2017-12-14 22:51:28 -0500

    PLAY IN THE SNOW WINTER UPDATE CONFIRMED

---
+++ Assets/Scripts/Camron's stuff/MeshDeformer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+
+    //mesh data
+    Mesh deformingMesh;
+    Vector3[] originalVertices, displacedVertices;
+
+    Vector3[] vertexVelocities;
+
+    //starting data
+    public float startHeight;
+    public float recoverySpeed;
+    public float downSpeed;
+    public float cutoffEffectDistance;
+    public bool enableDoubleSquareDeformDistance;
+
+
+    // Use this for initialization
+    void Start () {
+        //set mesh data
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++) {
+            displacedVertices[i] = originalVertices[i];
+        }
+        vertexVelocities = new Vector3[originalVertices.Length];
+
+        //so we can compare to square magnitute of vectors rather than magnitute. 
+        cutoffEffectDistance *= cutoffEffectDistance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //update each vertex location
+        for (int i = 0; i < displacedVertices.Length; i++) {
+            if (displacedVertices[i].y < originalVertices[i].y) {
+                displacedVertices[i] += new Vector3(0.0f, recoverySpeed * Time.deltaTime, 0.0f);
+            }
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+    }
+
+
+    public void FlattenPoint(Vector3 point) {
+        point -= transform.position;
+        for (int i = 0; i < displacedVertices.Length; i++) {
+            Vector3 pointToVertex = displacedVertices[i] - point;
+            float dis = pointToVertex.sqrMagnitude;
+            if (dis < cutoffEffectDistance) {
+                if (enableDoubleSquareDeformDistance)
+                    dis *= dis;
+                float scaledDistance = downSpeed * Time.deltaTime / (1f + dis * dis);
+                displacedVertices[i] = new Vector3(displacedVertices[i].x, Mathf.Max(displacedVertices[i].y - scaledDistance, originalVertices[i].y - 1.0f), displacedVertices[i].z);
+                deformingMesh.vertices = displacedVertices;
+                deformingMesh.RecalculateNormals();
+            }
+        }
+    }
+
+
+    /*public void AddDeformingForce(Vector3 point, float force) {
+        for (int i = 0; i < displacedVertices.Length; i++) {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point, float force) {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }*/
+        
+
+
+}
---
+++ Assets/Scripts/Camron's stuff/MeshDeformerContact.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformerContact : MonoBehaviour {
+
+    MeshDeformer deformer;
+    public float requiredSpeed;
+
+    // Use this for initialization
+    void Start () {
+        deformer = GetComponent<MeshDeformer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //constant collision detection
+    private void OnCollisionStay(Collision collision) {
+        //for each point of contact
+        foreach (ContactPoint contact in collision.contacts) {
+            //MeshDeformer deformer =  contact.otherCollider.GetComponent<MeshDeformer>();
+            if (deformer) {
+                Vector3 point = contact.point;
+                deformer.FlattenPoint(point);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider collision) {
+        if (deformer && collision.GetComponent<Rigidbody>() && collision.GetComponent<Rigidbody>().velocity.magnitude > requiredSpeed) {
+            Vector3 point = collision.transform.position;
+            deformer.FlattenPoint(point);
+        }
+    }
+
+}
