ace119222e9a24bebf5448323069994ff26192c0
Splatypus <lionheart365@gmail.com>
2017-12-26 13:11:14 -0700

    Added a generic interaction system

---
+++ Assets/Scripts/Camron's stuff/InteractableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+
+    //when a player enters, they can now interact with the object
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            other.GetComponent<PlayerInteractions>().addObject(this);
+        }
+    }
+
+    //when the player leaves, theyre no longer able to interact
+    public void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            other.GetComponent<PlayerInteractions>().removeObject(this);
+        }
+    }
+    
+    //overridable interact function
+    public virtual void OnInteract() {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Camron's stuff/PlayerInteractions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour {
+
+    List<InteractableObject> inRangeOf = new List<InteractableObject>();
+    public string InputAxis = "Interact";
+
+    //add object to things we can interact with
+    public void addObject(InteractableObject i) {
+        inRangeOf.Add(i);
+    }
+
+    //remove object from list
+    public void removeObject(InteractableObject i) {
+        inRangeOf.Remove(i);
+    }
+
+    //when interact button is pressed, interact with everything were in range of
+    public void Update() {
+        if (Input.GetButtonDown(InputAxis)) {
+            foreach (InteractableObject i in inRangeOf) {
+                i.OnInteract();
+            }
+        }
+    }
+}
