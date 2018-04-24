bac4755ce0b6b4c062d01d5a0df73b372fcddccf
Splatypus <lionheart365@gmail.com>
2018-04-24 18:47:41 -0400

    Backend stuff for blackie is mostly done

---
+++ Assets/Scripts/NPC stuff/Mingames/Blackie/BlackieMiniGame.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class BlackieMiniGame : Dialog {
+
+    public List<List<Gamepiece>> grid = new List<List<Gamepiece>>(); //grid of game pieces. First list is x values, second is y values
+    Gamepiece emptyPiece = new EmptyNode(); //same object reference is used for every empty space
+    public string[] puzzleFileNames;
+
+    //reads in a puzzle set-up from a file and starts that puzzle
+    public void ReadFromFile(string fileName) {
+        StreamReader reader = new StreamReader("Assets/Resources/BlackieGames/" + fileName + ".txt");
+        reader.Close();
+    }
+
+    //if the location x, y is within the bounds of the grid
+    public bool IsInBounds(int x, int y) {
+        return x >= 0 && y >= 0 && x < grid.Count && y < grid[0].Count; //&& (grid.Count == 0 ? false : y < grid[0].Count); Dont think I need this but grid[0] might cause issues if length 0
+    }
+
+    //returns the gamepiece at a specific location. Returns the empty piece if that location is not in bounds
+    public Gamepiece GetAtLocation(int x, int y) {
+        if (IsInBounds(x, y))
+            return grid[x][y];
+        else
+            return emptyPiece;
+    }
+
+    //generates an adjacency array based on the location and direction
+    public Gamepiece[] GenerateAdjacents(int x, int y, int direction) {
+        Gamepiece[] adjacents = new Gamepiece[4];
+        direction = 4 - direction;
+        adjacents[(direction + 0) % 4] = GetAtLocation(x, y - 1);
+        adjacents[(direction + 1) % 4] = GetAtLocation(x + 1, y);
+        adjacents[(direction + 2) % 4] = GetAtLocation(x, y + 1);
+        adjacents[(direction + 3) % 4] = GetAtLocation(x - 1, y);
+        return adjacents;
+    }
+
+    //True if the piece can be placed at x,y on the grid. False if it cannot be. Direction must be between 0 and 3. 0 is north then it goes clockwise
+    public bool CanPlace(Gamepiece p, int x, int y, int direction) {
+        if (IsInBounds(x, y) && grid[x][y] == emptyPiece) {
+            //adjacent squares set up so that 0 in the array is in front of the piece being placed, then clockwise
+            Gamepiece[] adjacents = GenerateAdjacents(x, y, direction);
+            //check if the piece can be placed there based on it's own requirements
+            return p.CanPlace(adjacents);
+        }
+        return false;
+    }
+
+    //places p on the grid at x,y, facing the given direction. 0 is north, clockwise to 3 is west.
+    public void PlacePiece(Gamepiece p, int x, int y, int direction) {
+        if (CanPlace(p, x, y, direction)) {
+            //add to grid and give the piece the information it needs. Then update its in world gameobject
+            grid[x][y] = p;
+            p.direction = direction;
+            //set p.worldobject position here
+
+            //set its power
+            Gamepiece[] adjacents = GenerateAdjacents(x, y, direction);
+            p.SetInitialPowerState(adjacents);
+            //then if this piece becomes powered, check all adjacent unpowered pieces and update them too, chain until no more update
+            if (p.IsPowered()) {
+                UpdatePowerState(x, y - 1);
+                UpdatePowerState(x + 1, y);
+                UpdatePowerState(x, y + 1);
+                UpdatePowerState(x - 1, y);
+            }
+        }
+    }
+
+    //used when a piece is placed to recursively update power of adjacent nodes
+    public void UpdatePowerState(int x, int y) {
+        Gamepiece p = GetAtLocation(x, y);
+        //if the piece at location x,y is not empty or out of bounds or powered, then make its adjacency array and update its power as if it was just placed
+        if (p != emptyPiece && !p.IsPowered()) {
+            Gamepiece[] adjacents = GenerateAdjacents(x, y, p.direction);
+            p.SetInitialPowerState(adjacents);
+            //if this causes p to become powered, keep iterating
+            if (p.IsPowered()){
+                UpdatePowerState(x, y - 1);
+                UpdatePowerState(x + 1, y);
+                UpdatePowerState(x, y + 1);
+                UpdatePowerState(x - 1, y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NPC stuff/Mingames/Blackie/Gamepiece.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamepiece {
+    
+    public enum PowerStates { Red, Blue, Off};
+    public PowerStates state = PowerStates.Off;
+    public int direction = 0; //the direction this piece is facing. 0 is north, going clockwise to 3 being west
+    public GameObject prefab;
+    public GameObject worldObject;
+
+    //if this piece can be placed based on what is adjacent to it. 
+    //Adjacents is an array of the 4 adjacent pieces, starting with the one north of this one and going clockwise
+    public virtual bool CanPlace(Gamepiece[] adjacents) {
+        return true;
+    }
+
+
+    //sets the power state of this node based on adjacent ones. Usually called right after it's placed, or when the power state of adjacent nodes change
+    public virtual void SetInitialPowerState(Gamepiece[] adjacents) {
+
+    }
+
+    //if there is anything connected to this piece
+    public bool IsPowered() {
+        return state != PowerStates.Off;
+    }
+
+    //sets the power state of the node. Changes its color or whatever it needs to do too
+    public void SetState(PowerStates newstate) {
+        state = newstate;
+    }
+}
+
+
+//node that has nothing that is sent with a bounds issue or an empty space. Is always unpowered. Stops me from having to check for null every time I look at adjacent nodes
+public class EmptyNode : Gamepiece { }
+
+//the standard node
+public class BlackieNode : Gamepiece{
+
+    //can only be places if no more than one adjacent node is powered
+    public override bool CanPlace(Gamepiece[] adjacents)
+    {
+        int numPowered = 0;
+        foreach (Gamepiece g in adjacents)
+        {
+            if (g.IsPowered())
+                numPowered++;
+        }
+        return numPowered <= 1;
+    }
+
+    //there should only be at most one powered node next to this one when this is called. Set it to the same power state as that one, or to off if there isnt another powered one
+    public override void SetInitialPowerState(Gamepiece[] adjacents)
+    {
+        //if theres an adjacent powered node, we want to match that
+        state = PowerStates.Off;
+        foreach (Gamepiece g in adjacents)
+        {
+            if (g.IsPowered())
+                SetState(g.state);
+        }
+    }
+}
+
+//takes two input and one output. If either input is the prefered state, so is the output.
+public class ColorGate : Gamepiece {
+    public PowerStates preferedState;
+
+}
+
+//takes one input and one output. Output is opposite of input
+public class Inverter : Gamepiece {
+
+}
