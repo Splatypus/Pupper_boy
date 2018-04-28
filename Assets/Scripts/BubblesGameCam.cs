96348f6a589f694e44428623661eebd3f0954336
Rikusaber <huange2@rpi.edu>
2018-04-28 11:22:01 -0400

    updated backyard

---
+++ Assets/Scripts/BubblesGameCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesGameCam : MonoBehaviour {
+
+    public GameObject playerCam;
+    public GameObject gameCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PlayerCharacter")
+        {
+            playerCam.SetActive(false);
+            gameCam.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PlayerCharacter")
+        {
+            playerCam.SetActive(true);
+            gameCam.SetActive(false);
+        }
+
+    }
+}
