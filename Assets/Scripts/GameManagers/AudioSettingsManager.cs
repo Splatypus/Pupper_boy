3ea923a2f6f8d0ede3305e57497ef992e51404f5
MyNESCreations <tyhbrid@gmail.com>
2018-10-05 03:17:04 -0400

    Moved Audio Filles, Added Audio Settings
    moved the audio files to help with orginization, added in audio settings so that the settings sliders do something now, and the mixer is used for all sounds. music sfx etc

---
+++ Assets/Scripts/GameManagers/AudioSettingsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioSettingsManager : MonoBehaviour {
+
+    //Main AudioMixer
+    public AudioMixer masterMixer;
+
+    //The Sliders
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    void Start() {
+
+        //mute music at the start to account for needed delay
+        masterMixer.SetFloat("musicVol", -60f);
+
+        //Set references to self for the Sliders
+        masterSlider.GetComponent<PlayerPrefsManager>().myManager = this;
+        musicSlider.GetComponent<PlayerPrefsManager>().myManager = this;
+        sfxSlider.GetComponent<PlayerPrefsManager>().myManager = this;
+    }
+
+    public void ChangeMixerVolume(string mixKey, float value) {
+        masterMixer.SetFloat(mixKey, value);
+    }
+}
--- Assets/Scripts/GameManagers/PlayerPrefsManager.cs
+++ Assets/Scripts/GameManagers/PlayerPrefsManager.cs
@@ -8,33 +8,54 @@ public class PlayerPrefsManager : MonoBehaviour {
     //The Key To Change
     public string key = "";
 
-    public GameObject myObject;
+    //Set by SettingsManager for referencing
+    [HideInInspector]
+    public AudioSettingsManager myManager;
 
-    void Start()
-    {
+    //only allow access for reading my value, loading from save
+    private float myValue;
+    public float myValueGet { get { return myValue; } }
+
+    //Not Needed anymore, but kept for continuity
+    GameObject myObject;
+
+    //Needed delay because references were not being set fast enough (don't remember if there is a better way to do this)
+    void Start() {
+        Invoke("StartDelayed", .1f);
+    }
+
+    void StartDelayed() {
         //Make sure that the key exists
-        if (PlayerPrefs.HasKey(key))
-        {
+        if (PlayerPrefs.HasKey(key)) {
             if (!myObject)
                 myObject = this.gameObject;
 
-            //check to see if its a slider, and if it is, load the value
+            //load the value to the slider
             if (myObject.GetComponent<Slider>())
-                myObject.GetComponent<Slider>().normalizedValue = PlayerPrefs.GetFloat(key);
+                myValue = myObject.GetComponent<Slider>().value = PlayerPrefs.GetFloat(key);
+
+            myManager.ChangeMixerVolume(key, myValue);
         }
     }
 
+    //Updating the PlayerPrefs value to slider.value
     public void UpdateSettingFloat(float value) {
         PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
+        myValue = value;
+
+        myManager.ChangeMixerVolume(key, value);
     }
 
-    public void UpdateSettingInt(int value)
-    {
-        PlayerPrefs.SetInt(key, value);
-    }
+    //Not Used Anymore but could be helpful for later
 
-    public void UpdateSettingString(string value)
-    {
-        PlayerPrefs.SetString(key, value);
-    }
+    //public void UpdateSettingInt(int value)
+    //{
+    //    PlayerPrefs.SetInt(key, value);
+    //}
+
+    //public void UpdateSettingString(string value)
+    //{
+    //    PlayerPrefs.SetString(key, value);
+    //}
 }
