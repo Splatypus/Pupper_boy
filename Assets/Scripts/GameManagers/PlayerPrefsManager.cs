4433a662fd9772e7df08eaca322d48ab42c672cc
MyNESCreations <tyhbrid@gmail.com>
2018-10-04 22:56:05 -0400

    Updates to DankAssTitleScreen and New Script
    I updated the layout of DankAssTitleScreen, and I also added a new script that allows you to save and load player prefs for specific objects, currently working with Sliders, but easy to add functionality to other object types.

---
+++ Assets/Scripts/GameManagers/PlayerPrefsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    //The Key To Change
+    public string key = "";
+
+    public GameObject myObject;
+
+    void Start()
+    {
+        //Make sure that the key exists
+        if (PlayerPrefs.HasKey(key))
+        {
+            if (!myObject)
+                myObject = this.gameObject;
+
+            //check to see if its a slider, and if it is, load the value
+            if (myObject.GetComponent<Slider>())
+                myObject.GetComponent<Slider>().normalizedValue = PlayerPrefs.GetFloat(key);
+        }
+    }
+
+    public void UpdateSettingFloat(float value) {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    public void UpdateSettingInt(int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public void UpdateSettingString(string value)
+    {
+        PlayerPrefs.SetString(key, value);
+    }
+}
