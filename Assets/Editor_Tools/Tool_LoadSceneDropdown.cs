71ed9adaa7d6d84ac77f66499dab21f6eb578f53
MyNESCreations <tyhbrid@gmail.com>
2018-10-07 20:25:22 -0400

    Added a quick nice tool for openeing specific scenes
    plus one to open backyard and holloween at the same time

---
+++ Assets/Editor_Tools/Tool_LoadSceneDropdown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+public class Tool_LoadSceneDropdown : MonoBehaviour {
+
+    [MenuItem("Tools/Scene Loading/Load Title Screen &#t")]
+    private static void LoadTitleScreen() {
+        EditorSceneManager.OpenScene("Assets/Scenes/DankAssTitleScreen.unity");
+    }
+
+    [MenuItem("Tools/Scene Loading/Load Backyard &#b")]
+    private static void LoadBackyard() {
+        EditorSceneManager.OpenScene("Assets/Scenes/Backyard.unity");
+    }
+
+    [MenuItem("Tools/Seasonal Scene Loading/Load Halloween &#h")]
+    private static void LoadHalloween() {
+        EditorSceneManager.OpenScene("Assets/Scenes/Backyard.unity");
+        EditorSceneManager.OpenScene("Assets/Scenes/Seasonal Scenes/Halloween.unity", OpenSceneMode.Additive);
+    }
+}
