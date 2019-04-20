7612f7c31e9394547f3190c4809814222c640788
MyNESCreations <tyhbrid@gmail.com>
2019-04-20 00:06:31 -0600

    Updated Jumping. Added Float To Jump, so that it feels better.

--- Assets/Scripts/CharacterControllers/MovementController.cs
+++ Assets/Scripts/CharacterControllers/MovementController.cs
@@ -6,12 +6,21 @@ public class MovementController : Controller
 {
     private CharacterController myController;
     private readonly float gravity = -9.8f;
+    private bool isGrounded;
 
     private Vector3 movementDirection;
-    private Vector3 velocity;
+    private Vector3 velocity = Vector3.zero;
 
-    public float movementSpeed = 7.5f;
-    public float jumpForce = 3.5f;
+    public float movementSpeed = 10;
+    public float jumpForce = 1.1f;
+
+    private bool jumped = false;
+    private bool jumping = false;
+    private float jumpTimer = 0;
+    public float jumpBeginDuration = 0.2f;
+    public float jumpFloatDuration = 0.5f;
+    [Range(0.01f, 1)]
+    public float jumpFloatAmount = 0.4f;
 
     void Start()
     {
@@ -20,6 +29,8 @@ public class MovementController : Controller
 
     void FixedUpdate()
     {
+        isGrounded = myController.isGrounded;
+
         UpdateMovement();
 
         UpdateVelocity();
@@ -33,7 +44,7 @@ public class MovementController : Controller
 
         //Apply Values To Movement Direction
         movementDirection = cameraForward + cameraRight;
-        myController.Move(movementDirection.normalized * Time.deltaTime * movementSpeed);
+        myController.Move(movementDirection.normalized * Time.fixedDeltaTime * movementSpeed);
 
         //Apply Movement To Character If movementDirection Is Not 0
         if (movementDirection != Vector3.zero)
@@ -42,15 +53,70 @@ public class MovementController : Controller
 
     void UpdateVelocity()
     {
-        print(myController.isGrounded);
-        //Apply Gravity To Velocity
-        velocity.y += gravity * Time.deltaTime;
+        if (jumping)
+        {
+            JumpBegin();
+        }
+        else if (jumped)
+        {
+            JumpFloat();
+        }
+        else
+        {
+            //Apply Gravity To Velocity
+            velocity.y += gravity * Time.fixedDeltaTime;
 
-        //Apply Jump Force, If Jump Is Pressed, To Velocity
-        velocity.y += Input.GetButtonDown("Jump") ? jumpForce : 0;
+            //Apply Velocity To Character
+            myController.Move(velocity);
 
-        velocity.y -= gravity * Time.deltaTime;
-        //Apply Velocity To Character
+            //Readjust Gravity For If Player Is Grounded
+            velocity.y = myController.isGrounded ? 0 : velocity.y;
+
+            //Check For Jumping, Beginning Next Frame
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity = Vector3.zero;
+                jumping = true;
+            }
+        }
+    }
+
+    //Runs Durring the Top of the Jump Arc
+    void JumpFloat()
+    {
+        velocity.y -= Time.fixedDeltaTime * (1 / jumpFloatAmount);
         myController.Move(velocity);
+
+        jumpTimer += Time.fixedDeltaTime;
+
+        if (jumpTimer >= jumpFloatDuration)
+        {
+            jumpTimer = 0;
+            jumped = false;
+        }
+    }
+
+    //Runs Durring the Beginning of the Jump Arc
+    void JumpBegin()
+    {
+        velocity.y += jumpForce * Time.fixedDeltaTime;
+        myController.Move(velocity);
+
+        jumpTimer += Time.fixedDeltaTime;
+
+        if (jumpTimer >= jumpBeginDuration)
+        {
+            jumpTimer = 0;
+            jumping = false;
+            jumped = true;
+        }
+    }
+
+    void AddForce(Vector3 forceVector, bool isAddative = true)
+    {
+        if (!isAddative)
+            velocity = Vector3.zero;
+
+        velocity += forceVector;
     }
 }
