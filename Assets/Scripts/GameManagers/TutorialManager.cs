614acbb9a9f70bef7552319dc610614d93c3f118
Splatypus <lionheart365@gmail.com>
2019-06-26 22:41:29 -0400

    Updated tutorial popups

---
+++ Assets/Scripts/GameManagers/TutorialManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour {
+
+    public static TutorialManager instance;
+
+    public GameObject popup;
+    Text text;
+
+    public void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+        }
+    }
+
+    public void Start() {
+        text = popup.GetComponentInChildren<Text>();
+    }
+
+    public void EnableWithText(string t) {
+        popup.SetActive(true);
+        text.text = t;
+    }
+
+    public void DisableTutorial() {
+        popup.SetActive(false);
+    }
+
+}
