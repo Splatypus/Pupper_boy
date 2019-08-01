7e6a57f3b1c68a92bfe1ecac4758c0490883be01
Splatypus <lionheart365@gmail.com>
2019-08-01 10:53:30 -0400

    Replaced logo. Fixed an issue that caused the test save file to be generated from other scenes.

--- Assets/Scripts/GameManagers/Testing/SceneTestLoader.cs
+++ Assets/Scripts/GameManagers/Testing/SceneTestLoader.cs
@@ -12,6 +12,12 @@ public class SceneTestLoader : MonoBehaviour
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void LoadGameobject() {
+
+        //only create a test save if we're loading into the backyard with no save files
+        if (!UnityEngine.SceneManagement.SceneManager.GetActiveScene().name.Equals("UnleashedBackyard")) {
+            return;
+        }
+
         SaveManager save = SaveManager.getInstance();
 
         if (save.GetLastOpenID() != -1) {
