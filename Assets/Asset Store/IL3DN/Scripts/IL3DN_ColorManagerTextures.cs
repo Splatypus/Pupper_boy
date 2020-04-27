8e6eb3d27ac34dff983020fde0ff74f6ea120999
Splatypus <lionheart365@gmail.com>
2020-04-26 22:24:37 -0600

    New assets working with season change

--- Assets/Asset Store/IL3DN/Scripts/IL3DN_ColorManagerTextures.cs
+++ Assets/Asset Store/IL3DN/Scripts/IL3DN_ColorManagerTextures.cs
@@ -48,7 +48,7 @@
         {
             for (int i = 0; i < materials.Count; i++)
             {
-                if (materials[i].selectedProperty > 0 && materials[i].selectedProperty < materials[i].properties.Count)
+                if (materials[i].selectedProperty >= 0 && materials[i].selectedProperty < materials[i].properties.Count)
                 {
                     materials[i].meterial.color = materials[i].properties[materials[i].selectedProperty].color;
                     materials[i].meterial.mainTexture = materials[i].properties[materials[i].selectedProperty].mainTex;
@@ -60,9 +60,9 @@
         {
             for (int i = 0; i < materials.Count; i++)
             {
-                if (materials[i].properties.Count > slot - 1)
+                if (materials[i].properties.Count > slot)
                 {
-                    materials[i].selectedProperty = slot - 1;
+                    materials[i].selectedProperty = slot;
                 }
             }
             Refresh();
---
+++ Assets/Scripts/DayNight and Seasonal/IL3DNSeasonChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IL3DN;
+using UnityEngine;
+
+public class IL3DNSeasonChange : MonoBehaviour
+{
+
+    public IL3DN_ColorManagerTextures colorManager;
+    public IL3DN_Snow snowManager;
+
+
+    void Awake()
+    {
+        EventManager.OnSeasonChange += OnSeasonChanged;
+    }
+    
+    void OnDestroy()
+    {
+        EventManager.OnSeasonChange -= OnSeasonChanged;
+    }
+
+    // called when a season is changed
+    void OnSeasonChanged(SeasonManager.Seasons s)
+    {
+        //Set colors based on season
+        colorManager.SetMaterialColors((int)s);
+        //set snow to true in the winter
+        snowManager.Snow = (s == SeasonManager.Seasons.WINTER);
+    }
+}
